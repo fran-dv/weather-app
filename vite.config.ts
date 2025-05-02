a709cda5031c931a9579db1037fbe717181c04db
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-02 12:20:59 -0300

    feat: install tailwind for styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
 import path from "path";
+import tailwindcss from '@tailwindcss/vite'
 
 dotenv.config();
 
@@ -13,4 +14,7 @@ export default defineConfig({
   define: {
     "process.env": process.env,
   },
+  plugins: [
+    tailwindcss(),
+  ],
 });
