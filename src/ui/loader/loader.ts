26044faccabd9cf22afaa21b384af4073b59ff44
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-05 17:26:17 -0300

    fix: remove unused 'loading' variable

--- src/ui/loader/loader.ts
+++ src/ui/loader/loader.ts
@@ -1,9 +1,6 @@
 import { emptyMainDiv } from "@/ui";
 
-let loading: boolean;
-
 export const setLoading = (state: boolean) => {
-  loading = state;
   const loader = document.getElementById("loader");
 
   if (loader) {
