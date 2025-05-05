4c78918e838986c6e61082195a61117cdf9ec034
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-05 10:18:33 -0300

    feat: Implement weather info generator, which displays the data in WeatherData interface

--- src/ui/weather-info/weatherInfo.ts
+++ src/ui/weather-info/weatherInfo.ts
@@ -1,15 +1,214 @@
 import { WeatherData } from "@/core";
 import { generateDiv } from "@fran-dv/ui-components";
-import "./weather-info.css";
+import { Icons, iconsSvg } from "./weather-icons";
+import { format } from "date-fns";
 
 enum Classes {
-    InfoContainer = "weather-info",
+  InfoContainer = "weather-info",
+  section1 = "info-section-1",
+  iconDiv = "climate-icon",
+  feelsLikeDiv = "feelslike",
+  scaleToggle = "scale-toggle",
+  section2 = "info-section-2",
+  section3 = "info-section-3",
+  wrapper = "weather-info-wrapper",
+  InfoRow = "weather-info-row",
+  RealTemp = "real-temperature",
 }
+
+export enum Ids {
+  mainContainer = "main-content",
+}
+
+export enum DataClick {
+  toggleTempScale = "togle-scale",
+}
+
+export enum TempScales {
+  cs = "°C",
+  fh = "°F",
+}
+
+let currentScale: keyof typeof TempScales;
+
+export const toggleTempScale = () => {};
 
 export const generateWeatherInfoDiv = (data: WeatherData): HTMLDivElement => {
-    const container = generateDiv({ classes: [Classes.InfoContainer] })
+  const container = generateDiv({ classes: [Classes.InfoContainer] });
+  // container styles
+  container.classList.add(
+    "flex",
+    "flex-col",
+    "bg-gray-800",
+    "p-8",
+    "gap-8",
+    "rounded-lg",
+  );
 
-    
+  // first section
+  const firstSection = generateDiv({ classes: [Classes.section1] });
+  const feelsLikeDiv = generateDiv({ classes: [Classes.feelsLikeDiv] });
 
-    return container;
-}
+  const iconDiv = generateDiv({ classes: [Classes.iconDiv] });
+  const iconImg = document.createElement("img");
+  if (iconsSvg[data.icon as Icons]) {
+    iconImg.src = iconsSvg[data.icon as Icons] as string;
+    iconImg.alt = data.icon;
+  }
+  iconDiv.appendChild(iconImg);
+  feelsLikeDiv.appendChild(iconDiv);
+
+  currentScale = "cs" satisfies keyof typeof TempScales;
+  const tempToDisplay = data.feelslike.c.toFixed(2);
+  const tempH2 = document.createElement("h2");
+  tempH2.textContent = tempToDisplay;
+  feelsLikeDiv.appendChild(tempH2);
+  const scaleToggleDiv = generateDiv({
+    classes: [Classes.scaleToggle],
+    customAttrs: [
+      {
+        name: "click",
+        value: DataClick.toggleTempScale,
+      },
+    ],
+  });
+  const scaleH2 = document.createElement("h2");
+  scaleH2.textContent = TempScales[currentScale];
+  scaleH2.title = `Toggle scale to ${currentScale === "cs" ? TempScales.fh : TempScales.cs}`;
+  scaleToggleDiv.appendChild(scaleH2);
+  feelsLikeDiv.appendChild(scaleToggleDiv);
+  firstSection.appendChild(feelsLikeDiv);
+
+  const descriptionP = document.createElement("p");
+  descriptionP.textContent = data.description;
+  firstSection.appendChild(descriptionP);
+
+  // general styles
+  const textColor = "text-gray-100";
+  enum FontSizes {
+    H2 = "text-3xl",
+    H3 = "text-2xl",
+    P = "text-xl",
+  }
+
+  // first section styles
+  firstSection.classList.add("flex", "flex-col", "w-1/2");
+  iconDiv.classList.add("flex-shrink-0.2");
+  iconImg.classList.add("w-30");
+  feelsLikeDiv.classList.add("flex", "gap-2", "items-center");
+  tempH2.classList.add(textColor, FontSizes.H2, "font-semibold");
+  scaleH2.classList.add(
+    textColor,
+    FontSizes.H2,
+    "cursor-pointer",
+    "font-semibold",
+  );
+  descriptionP.classList.add(textColor, "text-lg");
+
+  // second section
+  const secondSection = generateDiv({ classes: [Classes.section2] });
+  const locationP = document.createElement("p");
+  locationP.textContent = data.location;
+  secondSection.appendChild(locationP);
+  const conditionsH2 = document.createElement("h2");
+  conditionsH2.textContent = data.conditions;
+  secondSection.appendChild(conditionsH2);
+  const dateAndHourH3 = document.createElement("h3");
+  dateAndHourH3.textContent = `${format(new Date(data.date), "eeee")} ${data.hour.split(":").slice(0, -1).join(":")}`;
+  secondSection.appendChild(dateAndHourH3);
+
+  // second section styles
+  secondSection.classList.add(
+    "w-1/2",
+    "flex",
+    "flex-col",
+    "items-end",
+    "justify-center",
+    "gap-5",
+  );
+  locationP.classList.add(textColor, FontSizes.P, "text-right");
+  dateAndHourH3.classList.add(textColor, FontSizes.H3, "text-right");
+  conditionsH2.classList.add(
+    textColor,
+    FontSizes.H2,
+    "font-semibold",
+    "text-right",
+  );
+
+  // wrapper for sections 1 and 2
+  const wrapper = generateDiv({ classes: [Classes.wrapper] });
+  wrapper.appendChild(firstSection);
+  wrapper.appendChild(secondSection);
+
+  // wrapper styles
+  wrapper.classList.add("flex", "gap-3");
+
+  // third section
+  const thirdSection = generateDiv({ classes: [Classes.section3] });
+  // - precipitation row
+  const precipRow = generateDiv({ classes: [Classes.InfoRow] });
+  const precipTitle = document.createElement("h3");
+  const precipData = document.createElement("h3");
+  precipTitle.textContent = "Precipitation";
+  precipData.textContent = `${data.precip.toFixed(2)}%`;
+  precipRow.appendChild(precipTitle);
+  precipRow.appendChild(precipData);
+  thirdSection.appendChild(precipRow);
+  // - humidity row
+  const humidityRow = generateDiv({ classes: [Classes.InfoRow] });
+  const humidityTitle = document.createElement("h3");
+  const humidityData = document.createElement("h3");
+  humidityTitle.textContent = "Humidity";
+  humidityData.textContent = `${data.humidity.toFixed(2)}%`;
+  humidityRow.appendChild(humidityTitle);
+  humidityRow.appendChild(humidityData);
+  thirdSection.appendChild(humidityRow);
+  // - temperature row
+  const tempRow = generateDiv({ classes: [Classes.InfoRow] });
+  const tempTitle = document.createElement("h3");
+  const tempData = document.createElement("h3");
+  tempData.classList.add(Classes.RealTemp);
+  tempTitle.textContent = "Temperature";
+  tempData.textContent = `${data.temperature.c.toFixed(2)}${TempScales.cs}`;
+  tempRow.appendChild(tempTitle);
+  tempRow.appendChild(tempData);
+  thirdSection.appendChild(tempRow);
+  // - windspeed row
+  const windspeedRow = generateDiv({ classes: [Classes.InfoRow] });
+  const windspeedTitle = document.createElement("h3");
+  const windspeedData = document.createElement("h3");
+  windspeedTitle.textContent = "Windspeed";
+  windspeedData.textContent = `${data.windspeed.toFixed(2)} km/h`;
+  windspeedRow.appendChild(windspeedTitle);
+  windspeedRow.appendChild(windspeedData);
+  thirdSection.appendChild(windspeedRow);
+
+  // third section styles
+  thirdSection.classList.add("flex", "flex-col");
+  const rows = thirdSection.querySelectorAll(`.${Classes.InfoRow}`);
+  let i = 0;
+  rows.forEach((row) => {
+    row.classList.add(
+      "flex",
+      "justify-between",
+      "pr-4",
+      "pl-4",
+      "pt-3",
+      "pb-3",
+      "border-gray-400",
+      "border-t",
+    );
+    if (i === rows.length - 1) {
+      row.classList.add("border-b");
+    }
+    ++i;
+  });
+  const rowTitles = thirdSection.querySelectorAll("h3");
+  rowTitles.forEach((title) => {
+    title.classList.add(textColor, FontSizes.H3, "text-semibold");
+  });
+
+  container.appendChild(wrapper);
+  container.appendChild(thirdSection);
+  return container;
+};
