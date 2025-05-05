7d8a177377c2c7845519677ad98c407e4fd865bb
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-05 10:16:41 -0300

    feat: Implement event delegation for clicks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
-import apiResponseToWeatherData from "./core/weather/get-weather-data";
 import "./index.css";
+import apiResponseToWeatherData from "./core/weather/get-weather-data";
 import { fetchWeatherDataByLocation } from "./infraestructure";
+import { DataClick, generateWeatherInfoDiv, Ids } from "./ui";
 
-const apiResponse = await fetchWeatherDataByLocation({ location: 'Chascomús' });
+const apiResponse = await fetchWeatherDataByLocation({ location: "chascomús" });
 const weatherData = apiResponse ? apiResponseToWeatherData(apiResponse) : null;
 
-console.log(weatherData)
+const weatherContainer = document.querySelector(`#${Ids.mainContainer}`);
+
+if (weatherData && weatherContainer) {
+  console.log(apiResponse);
+  const test = generateWeatherInfoDiv(weatherData);
+  weatherContainer.appendChild(test);
+} else if (!weatherData) {
+  console.error(
+    `Oops! didn't can fetch the data. Please try reloading the page`,
+  );
+} else if (!weatherContainer) {
+  console.error(
+    `An error ocurred selecting the element with id ${Ids.mainContainer}`,
+  );
+}
+
+type Target = HTMLElement | null;
+
+const handleClick = (e: MouseEvent) => {
+  if (e.target === null) {
+    return;
+  }
+
+  const target: Target = (e.target as HTMLElement).closest("[data-click]");
+
+  if (!target) {
+    return;
+  }
+
+  switch (target.dataset.click) {
+    case DataClick.toggleTempScale:
+      break;
+  }
+};
+
+const clickHandler = () => handleClick;
+
+document.addEventListener("click", clickHandler);
