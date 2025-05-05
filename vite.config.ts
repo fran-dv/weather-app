74a21e64e388f258ccbb11d2787110e9733d2423
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-05 17:23:31 -0300

    Update configs to gh-deploys

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
-import path from "path";
+import path, { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 dotenv.config();
 
 export default defineConfig({
+  base: "/weather-app/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html")
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
